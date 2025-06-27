6a93f374c64cc6f112625947f8a3a130e64e7219
Rittik Bhowmick <143105527+beat57@users.noreply.github.com>
2025-06-27 16:32:18 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -7,38 +7,28 @@ import tweetRoute from "./routes/tweetRoute.js";
 import cors from "cors";
 
 dotenv.config({
-    path: ".env"
-});
+    path:".env"
+})
 databaseConnection();
 const app = express(); 
 
 // middlewares  
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended:true
+}));
 app.use(express.json());
 app.use(cookieParser()); 
-
-const allowedOrigins = [
-    "https://mern-social-media-website.onrender.com",
-    "http://localhost:3000" // For local development
-];
-
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-};
-
+    origin:"http://localhost:3000",
+    credentials:true
+}
 app.use(cors(corsOptions));
 
 // api
-app.use("/api/v1/user", userRoute);
+app.use("/api/v1/user",userRoute);
 app.use("/api/v1/tweet", tweetRoute);
  
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening at port ${process.env.PORT}`);
+
+app.listen(process.env.PORT,() => {
+    console.log(`Server listen at port ${process.env.PORT}`);
 });
